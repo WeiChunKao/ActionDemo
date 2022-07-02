366a82b4a7510a417ee236d5b3a57fc22cc0de2f
WeiChunKao <34979078+WeiChunKao@users.noreply.github.com>
2022-07-02 22:35:25 +0800

    加入專案檔案。

---
+++ ActionDemoTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace ActionDemoTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase(1, 1, 2)]
+        [TestCase(12, 30, 42)]
+        [TestCase(14, 1, 15)]
+        public void Test_Add(int a,int b, int expected)
+        {
+            //===Arrange===
+            var actual = a + b;
+            //===Assert===
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
